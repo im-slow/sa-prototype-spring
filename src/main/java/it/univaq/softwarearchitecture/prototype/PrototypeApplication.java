b648734834a876ffccaeeb2f690544fa5f1073f5
im-slow <andrea.amicosante@gmail.com>
2021-02-20 11:47:40 +0100

    Initial commit

---
+++ src/main/java/it/univaq/softwarearchitecture/prototype/PrototypeApplication.java
@@ -0,0 +1,21 @@
+package it.univaq.softwarearchitecture.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PrototypeApplication {
+
+    @GetMapping("/")
+    public String test() {
+        return "Servizio T1";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeApplication.class, args);
+    }
+
+}
