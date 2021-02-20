8fa2091dae71e04d61793493a355fef79296dece
im-slow <andrea.amicosante@gmail.com>
2021-02-20 12:15:59 +0100

    test

--- src/main/java/it/univaq/softwarearchitecture/prototype/PrototypeApplication.java
+++ src/main/java/it/univaq/softwarearchitecture/prototype/PrototypeApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PrototypeApplication {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String test() {
         return "Servizio T1";
     }
